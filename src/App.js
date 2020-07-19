afff78b16c295cd66fb8955a5df500d5e283ec71
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2020-07-19 20:52:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { AlbumList } from 'pages/AlbumList'
 import { ArtistDetail } from 'pages/ArtistDetail'
+import { Link } from 'react-router-dom'
 
 export const App = () => {
   return (
@@ -11,7 +12,7 @@ export const App = () => {
           <AlbumList />
         </Route>
         <Route path="/artists/:artistId">
-          <ArtistDetail />
+                     <ArtistDetail />
         </Route>
       </Switch>
     </BrowserRouter>
